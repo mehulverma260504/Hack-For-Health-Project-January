88a69e05172e02ae85ce2e8409802e66dfb0ada3
Mehul Verma <mehulverma04@gmail.com>
2024-01-13 17:17:44 +0530

    jinja changes

--- app.py
+++ app.py
@@ -30,9 +30,13 @@ def predict(values, dic):
         values = np.asarray(values)
         return model.predict(values.reshape(1, -1))[0]
 
+# @app.route("/")
+# def home():
+#     return render_template('home.html')
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index1.html')
 
 @app.route("/diabetes", methods=['GET', 'POST'])
 def diabetesPage():
