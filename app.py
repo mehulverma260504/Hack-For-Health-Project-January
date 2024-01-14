51c43f5fcdf68b95f0c07b5e1676bf1100192595
Mehul Verma <mehulverma04@gmail.com>
2024-01-14 22:57:18 +0530

    changing a few things

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
 
-
 app = Flask(__name__)
-
 
 def predict(values, dic):
     if len(values) == 8:
@@ -30,61 +28,45 @@ def predict(values, dic):
         values = np.asarray(values)
         return model.predict(values.reshape(1, -1))[0]
 
-
 @app.route("/")
 def home():
     return render_template("index1.html")
-
 
 @app.route("/contactus")
 def contactUs():
     return render_template("contact.html")
 
-
 @app.route("/aboutus")
 def aboutUs():
     return render_template("about-us.html")
-
 
 @app.route("/diabetes", methods=["GET", "POST"])
 def diabetesPage():
     return render_template("diabetes.html")
 
-
 @app.route("/diabetes1", methods=["GET", "POST"])
 def diabetes1Page():
     return render_template("diabetes1.html")
-
 
 @app.route("/cancer", methods=["GET", "POST"])
 def cancerPage():
     return render_template("breast_cancer.html")
 
-
 @app.route("/heart", methods=["GET", "POST"])
 def heartPage():
     return render_template("heart.html")
-
 
 @app.route("/kidney", methods=["GET", "POST"])
 def kidneyPage():
     return render_template("kidney.html")
 
-
 @app.route("/liver", methods=["GET", "POST"])
 def liverPage():
     return render_template("liver.html")
 
-
 @app.route("/malaria", methods=["GET", "POST"])
 def malariaPage():
     return render_template("malaria.html")
-
-
-@app.route("/pneumonia", methods=["GET", "POST"])
-def pneumoniaPage():
-    return render_template("pneumonia.html")
-
 
 @app.route("/predict", methods=["POST", "GET"])
 def predictPage():
@@ -98,7 +80,6 @@ def predictPage():
         return render_template("home.html", message=message)
 
     return render_template("predict.html", pred=pred)
-
 
 @app.route("/malariapredict", methods=["POST", "GET"])
 def malariapredictPage():
@@ -116,25 +97,6 @@ def malariapredictPage():
             message = "Please upload an Image"
             return render_template("malaria.html", message=message)
     return render_template("malaria_predict.html", pred=pred)
-
-
-@app.route("/pneumoniapredict", methods=["POST", "GET"])
-def pneumoniapredictPage():
-    if request.method == "POST":
-        try:
-            if "image" in request.files:
-                img = Image.open(request.files["image"]).convert("L")
-                img = img.resize((36, 36))
-                img = np.asarray(img)
-                img = img.reshape((1, 36, 36, 1))
-                img = img / 255.0
-                model = load_model("models/pneumonia.h5")
-                pred = np.argmax(model.predict(img)[0])
-        except:
-            message = "Please upload an Image"
-            return render_template("pneumonia.html", message=message)
-    return render_template("pneumonia_predict.html", pred=pred)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
